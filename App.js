ab6199edaed833173841b4f81398f2141c4e1e80
himanshu <himanshumirwal@gmail.com>
2022-05-15 15:46:13 +0530

    CORS message

--- App.js
+++ App.js
@@ -7,14 +7,7 @@ const app = express();
 require('dotenv').config();
 
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
-  if (req.method == "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use(express.static('public'));
