94c32c2dfa3d4fa9b5f9e19bd58ffadd348b1d7b
himanshu <himanshumirwal@gmail.com>
2022-05-15 15:24:33 +0530

    CORS message

--- App.js
+++ App.js
@@ -1,22 +1,20 @@
 const express = require('express');
-const app = express();
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
 const cors = require("cors");
+const app = express();
+
 require('dotenv').config();
-app.use((req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "OPTIONS, GET, POST, PUT, PATCH, DELETE"
-    );
-    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    if (req.method === "OPTIONS") {
-      return res.sendStatus(200);
-    }
-    next();
-  });
+
 app.use(cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST,GET,OPTIONS");
+  if (req.method == "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // app.use(express.static('public'));
